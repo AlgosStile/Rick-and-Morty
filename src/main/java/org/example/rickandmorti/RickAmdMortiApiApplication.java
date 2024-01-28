31187097b4e285c889bac7db0ed04a79672019de
AlgosStile <oleg.todor@mail.ru>
2024-01-28 09:19:11 +0300

    added Rick and Morty Characters App

---
+++ src/main/java/org/example/rickandmorti/RickAmdMortiApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.rickandmorti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAmdMortiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAmdMortiApiApplication.class, args);
+    }
+
+}
